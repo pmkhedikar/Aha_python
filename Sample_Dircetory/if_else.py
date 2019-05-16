cd5a218b75d479afbfe1d1aee17670013fbac5cc
pmkhedikar <pmkhedikar@gmail.com>
2019-05-16 15:22:49 +0530

    second commit

--- Sample_Dircetory/if_else.py
+++ Sample_Dircetory/if_else.py
@@ -1,5 +1,5 @@
-print("Guess number between 1 to 10 :")
-guess=int(input())
+# print("Guess number between 1 to 10 :")
+# guess=int(input())
 
 #if guess < 5:
 #    print("please guess the higher number")
@@ -18,16 +18,28 @@ guess=int(input())
 #else:
 #    print("Great ,you have gussed correct number {0} in first attempt".format(guess))
 
-if guess != 5:
-    if guess > 5:
-        print("Guess lower")
-    else:
-        print("Guess higher")
-    guess=int(input())
-    if guess==5:
-        print("You guess correct")
-    else:
-        print("Sorry ")
+# if guess != 5:
+#     guess=int(input())
+#     if guess==5:
+#         print("You guess correct")
+#     else:
+#         print("Sorry ")
+# else:
+#     if guess > 5:
+#         print("Guess lower")
+#     else:
+#         print("Guess higher")
+#     print("You guessed on first attempt")
+
+a=int(input("Enter your age :"))
+if (a<18) and (a>65):
+    print("Your are eligible for work")
 else:
-    print("You guessed on first attempt")
+    print("You have to wait for {} years".format(18 - a))
+
+
+
+
+
+
 
---
+++ Sample_Dircetory/if_else_2.py
@@ -0,0 +1,8 @@
+name=str(input("Enter your name :"))
+age=int(input("Eneter your age :"))
+if (age >=18) and (age <= 32):
+    print('Hi ,{0} Your are welcome for Holiday'.format(name))
+else:
+    print("Sorry our package does match with your requirement")
+
+
