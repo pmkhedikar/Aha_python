b0da4a2b516207e69d33df80927d1eeb3e817d05
pmkhedikar <pmkhedikar@gmail.com>
2019-05-20 15:19:22 +0530

    rename the files

---
+++ Sample_Dircetory/2_if_else_1.py
@@ -0,0 +1,48 @@
+print("Guess number between 1 to 10 :")
+guess=int(input())
+
+# 1 program
+if guess < 5:
+   print("please guess the higher number")
+   guess=int(input())
+   if guess == 5:
+       print("Now,you have guessed correct number")
+   else:
+       print("Sorry you have guessed wrong number")
+elif guess > 5:
+   print("please guess the lower number")
+   guess=int(input())
+   if guess == 5:
+       print("Now ,you have guessed correct number")
+   else:
+       print("Sorry you have gussed incorrect number")
+else:
+   print("Great ,you have gussed correct number {0} in first attempt".format(guess))
+
+
+# 2 program
+if guess != 5:
+    guess=int(input())
+    if guess==5:
+        print("You guess correct")
+    else:
+        print("Sorry ")
+else:
+    if guess > 5:
+        print("Guess lower")
+    else:
+        print("Guess higher")
+    print("You guessed on first attempt")
+
+a=int(input("Enter your age :"))
+if (a<18) and (a>65):
+    print("Your are eligible for work")
+else:
+    print("You have to wait for {} years".format(18 - a))
+
+
+
+
+
+
+
---
+++ Sample_Dircetory/2_if_else_3.py
@@ -0,0 +1,11 @@
+n = int(input("Enter any no: "))
+
+if n % 2 != 0:
+    print('Its odd no')
+elif(n % 2 == 00) and (2 <= n <= 5):
+     print('Its even no between 2 to 5')
+elif (n % 2 == 00) and (6 <= n <= 20):
+    print("Its even no between 6 to 20")
+else:
+    print('Its even no more than 20')
+
